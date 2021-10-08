1de7e7ddfd5169e26563fc937d892e55e07a7b16
Ricky Pardomuan Hutagalung <67309796+rickyhuta15@users.noreply.github.com>
2021-10-08 21:43:11 +0700

    commit

---
+++ Problem6/Script/SpawnKotak.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnKotak : MonoBehaviour
+{
+    public GameObject Kotak;
+
+    float randVert, randHoriz, randTotal;
+    void Start()
+    {
+        randTotal = Random.Range(5, 20);
+        for (int i = 0; i < randTotal; i++)
+        {
+            randVert = Random.Range(3.37f, -3.37f);
+            randHoriz = Random.Range(6.32f, -6.32f);
+            Instantiate(Kotak, new Vector2(randHoriz, randVert), Quaternion.identity);
+        }
+    }
+}
