a02f81689b6403902cf345a1bb7e791e7a52713c
Ricky Pardomuan Hutagalung <67309796+rickyhuta15@users.noreply.github.com>
2021-10-09 03:05:46 +0700

    commit

---
+++ Assets/Script/Score/ScoreScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScript : MonoBehaviour
+{
+    private Text scoreText;
+    public static int score = 0;
+    
+    public static void AddScore(int value)
+    {
+        score += value;
+    }
+}
