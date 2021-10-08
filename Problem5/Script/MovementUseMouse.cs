934d14232209d6227362e0821625596f27e1d77a
Ricky Pardomuan Hutagalung <67309796+rickyhuta15@users.noreply.github.com>
2021-10-08 21:56:15 +0700

    commit

---
+++ Problem5/Script/MovementUseMouse.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementUseMouse : MonoBehaviour
+{
+    public float speed = 5f;
+
+    Vector2 movement;
+    private Rigidbody2D rigidBody2D;
+    void Start()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+    void Movement()
+    {
+        Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        Vector2 position = transform.position;
+        cursorPos = cursorPos.normalized * speed * Time.deltaTime;
+        rigidBody2D.MovePosition(position + cursorPos);
+
+    }
+}
